2e7017cc5d88c5fa1f8b8d210311e2989a0f78a0
Kaushal Kumar Singh <kaushals.mca24@nitp.ac.in>
2025-10-26 11:17:39 +0530

    Update chatmod.py
    remove API code

--- chatmod.py
+++ chatmod.py
@@ -1,9 +1,9 @@
 from google import genai
 import os
 
-client = genai.Client(api_key=os.getenv("AIzaSyBCrzYyXY51JPvbVZJGRIE3W9b8IW4th5U"))
+client = genai.Client(api_key=os.getenv(""))
 
-uploaded_file = client.files.upload(file="/home/kaushalhero/Documents/gemini/bot/02 Ugi Ugi Suruj Dev.mp3") #mp3,pdf , mp4 , etc
+uploaded_file = client.files.upload(file="enter file path") #mp3,pdf , mp4 , etc
 
 response = client.models.generate_content(
     modal="gemini-2.0-flash",
